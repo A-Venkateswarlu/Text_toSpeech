bead6e3d4de6f3b41c8326b9c8aedaad86f9b682
A-Venkateswarlu <akkayyagariashok@gmail.com>
2025-08-03 12:44:08 +0530

    project

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, send_file, render_template
+from gtts import gTTS
+import os
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/speak', methods=['POST'])
+def speak():
+    data = request.json
+    text = data.get('text')
+    lang = data.get('lang', 'en')
+
+    tts = gTTS(text=text, lang=lang)
+    tts.save("output.mp3")
+
+    return send_file("output.mp3", as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
